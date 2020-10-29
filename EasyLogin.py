b4230b9b1a6b3c5bd1ac765c4d790839db07385e
Matheus Arnold Richardo <51102562+matheusarnold@users.noreply.github.com>
2020-10-29 21:22:55 +0700

    Update #1

--- EasyLogin.py
+++ EasyLogin.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 import webbrowser
 import os.path
 
@@ -21,6 +25,17 @@ password.send_keys(passwordStr)
 
 signInButton = driver.find_element_by_id('btnSubmit')
 signInButton.click()
+
+timeout = 20
+
+try:
+    WebDriverWait(driver, 
+    timeout).until(EC.visibility_of_element_located((By.XPATH, 
+    '//*[@id="studentViconList"]/tbody')))
+
+except TimeoutException:
+    print("Timed out waiting for page to load")
+    driver.quit()
 
 driver.implicitly_wait(5)
 webbrowser.open(driver.find_element_by_xpath('//*[@id="studentViconList"]/tbody/tr[3]/td[12]/div/a').get_attribute('href'))
