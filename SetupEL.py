cdd61dce78fc597a91052f952a771ba7af4def32
Matheus Arnold Richardo <51102562+matheusarnold@users.noreply.github.com>
2020-11-02 10:08:10 +0700

    Update #1
    Update ini memberikan command tambahan 'pip3 install -U selenium' jika command sebelumnya gagal.
    
    Update ini juga memberikan dokumentasi agar pengguna dapat memahami program, sehingga pengguna tidak perlu merasa khawatir karena tidak memahami programnya.

--- SetupEL.py
+++ SetupEL.py
@@ -1,14 +1,15 @@
-import subprocess
+import subprocess                                           # Untuk memasukkan subprocess library
 
-subprocess.call('pip install selenium', shell = True)
+subprocess.call('pip install selenium', shell = True)       # Untuk membuka CMD / shell, lalu jalankan command 'pip install selenium'
+subprocess.call('pip3 install -U selenium', shell = True)   # Kalo gagal, program akan coba pakai command ini
 
-username = input("\nEmail (Tanpa @binus.ac.id): ")
-password = input("Password: ")
+username = input("\nEmail (Tanpa @binus.ac.id): ")          # Meminta user untuk memasukkan email Binusmaya
+password = input("Password: ")                              # Meminta user untuk memasukkan password Binusmaya
 
-f = open("Credential.txt", "w+")
+f = open("Credential.txt", "w+")                            # Create and open file Credential.txt
 
-f.write(username)
-f.write("\n")
-f.write(password)
+f.write(username)                                           # Tulis isi variable username di Credential.txt baris pertama
+f.write("\n")                                               # Kasih enter
+f.write(password)                                           # Tulis isi variable password di Credential.txt baris kedua
 
-f.close()
+f.close()                                                   # Close Credential.txt
